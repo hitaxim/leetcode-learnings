e42077880ee00bb971a872b45747c352184ac621
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-09-17 10:59:20 -0700

    Create Restaurant Growth.sql

---
+++ sql-questions/Restaurant Growth.sql
@@ -0,0 +1,10 @@
+SELECT DISTINCT visited_on, SUM(amount) OVER CTE AS amount,
+ROUND((SUM(amount) OVER CTE)/7,2) AS average_amount
+FROM Customer
+
+
+WINDOW CTE AS (
+   ORDER BY visited_on
+   RANGE BETWEEN INTERVAL 6 DAY PRECEDING AND CURRENT ROW
+)
+   LIMIT 6, 999
