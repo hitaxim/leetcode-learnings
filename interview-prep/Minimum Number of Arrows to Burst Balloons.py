bde01c3c0f2aefe8c83feafaf06d819453653eae
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-09-27 14:16:28 -0700

    Create Minimum Number of Arrows to Burst Balloons.py

---
+++ interview-prep/Minimum Number of Arrows to Burst Balloons.py
@@ -0,0 +1,14 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if not points:
+            return 0
+        ans_arrow = 1
+        points.sort(key=lambda x: x[1])
+
+        end = points[0][1]
+        for p in points: 
+            start, end_balloon = p
+            if start > end: 
+                ans_arrow += 1
+                end = end_balloon
+        return ans_arrow
