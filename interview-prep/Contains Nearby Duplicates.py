9d9041bad09cd7c9c3be7f4d1d008b374ddf21f7
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-09-26 18:28:37 -0700

    Create Contains Nearby Duplicates.py

---
+++ interview-prep/Contains Nearby Duplicates.py
@@ -0,0 +1,8 @@
+class Solution:
+   def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+       ans = {}
+       for i, val in enumerate(nums):
+           if val in ans and abs(i - ans[val]) <= k:
+               return True
+           else:
+               ans[val] = i
