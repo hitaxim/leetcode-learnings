5df8226ec8e715cd98079a314532f7dca35a123a
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-09-27 14:43:17 -0700

    Create Non-Overlapping Intervals.py

---
+++ interview-prep/Non-Overlapping Intervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        ans = 0
+        intervals.sort(key=lambda x: x[1])
+        prev_end = intervals[0][1]
+
+        for i in range(1, len(intervals)):
+            if prev_end > intervals[i][0]:
+                ans += 1
+            else:
+                prev_end = intervals[i][1]
+
+        return ans
