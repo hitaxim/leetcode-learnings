1e6c80d46b66d38f184c3976ddc50cd7c5b68db1
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-10-01 09:49:26 -0700

    Create  Check if Balanced Binary Tree.py

---
+++ interview-prep/Check if Balanced Binary Tree.py
@@ -0,0 +1,16 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def dfs(root):
+            if not root: 
+                return [True, 0]
+            l, r = dfs(root.left), dfs(root.right)
+            balanced = l[0] and r[0] and abs(l[1] - r[1]) <= 1
+            return [balanced, 1+ max(l[1], r[1])]
+        
+        return dfs(root)[0]
