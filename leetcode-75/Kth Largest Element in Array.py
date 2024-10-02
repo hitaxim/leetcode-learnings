fc4f59a5381a0fedea583997edb94098bf8b4c8f
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-10-01 18:20:34 -0700

    Create Kth Largest Element in Array.py

---
+++ leetcode-75/Kth Largest Element in Array.py
@@ -0,0 +1,10 @@
+import heapq
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = nums[:k]
+        heapq.heapify(heap)
+        for i in range(k, len(nums)):
+            if nums[i] > heap[0]:
+                heapq.heappop(heap)
+                heapq.heappush(heap, nums[i])
+        return heap[0]
