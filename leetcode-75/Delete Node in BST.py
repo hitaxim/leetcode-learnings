0fcd0ed8db662d13d4f7660168c6bad14668f064
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-10-01 18:46:39 -0700

    Update Delete Node in BST.py

--- leetcode-75/Delete Node in BST.py
+++ leetcode-75/Delete Node in BST.py
@@ -1,1 +1,30 @@
-dgdfh
+class Solution:
+    def deleteNode(self, root: Optional[TreeNode], key: int) -> Optional[TreeNode]:
+        if not root: 
+            return None
+
+        if key < root.val: 
+            root.left = self.deleteNode(root.left, key)
+        elif key > root.val: 
+            root.right = self.deleteNode(root.right, key)
+        else: 
+            if not root.left and not root.right: 
+                return None
+            if not root.left: 
+                return root.right
+            if not root.right: 
+                return root.left
+            
+            root.val = self.findMin(root.right)
+            root.right = self.deleteNode(root.right, root.val)
+
+        return root
+
+    def findMin(self, root:TreeNode) -> int: 
+        while root.left: 
+            root = root.left
+        return root.val
+        
+
+
+
