7d0f564156b44fd61b5c36a8ac96f52ac62c980c
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-10-03 16:49:58 -0700

    Create Reverse Words in List.py

---
+++ leetcode-75/Reverse Words in List.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        words = s.split()
+        ans = []
+        for i in range(len(words)-1, -1, -1):
+            ans.append(words[i])
+
+        return " ".join(ans)
+
+"""
+return " ".join(s.strip().split()[::-1])
+"""
