4637cb84667f31edee871788313e01577ecc12a7
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-10-05 20:22:26 -0700

    Update Remove Duplicates From Sorted Array II.py

--- interview-prep/Remove Duplicates From Sorted Array II.py
+++ interview-prep/Remove Duplicates From Sorted Array II.py
@@ -9,3 +9,15 @@ class Solution:
                nums[index] = nums[i]
                index += 1
        return index
+
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        pos = 2
+        for i in range(2, len(nums)):
+            if nums[i] != nums[pos - 2]:
+                nums[pos] = nums[i]
+                pos += 1
+        return pos 
+        
