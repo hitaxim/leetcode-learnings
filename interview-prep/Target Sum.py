873061ca046d63221a9701acd0a121ded29081fa
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-10-06 19:58:59 -0700

    Update Target Sum.py

--- interview-prep/Target Sum.py
+++ interview-prep/Target Sum.py
@@ -1,5 +1,22 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        summ = sum(nums)
+        if summ < abs(target) or (summ + target) & 1 : 
+            return 0
+        
+        def knapsack(target):
+            dp = [1] + [0] * summ
+            for num in nums:
+                for j in range(summ, num-1, -1):
+                    dp[j] += dp[j - num]
+            return dp[target]
+        
+        return knapsack((summ + target) //2)
+
+
+"""
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
         l=defaultdict(int)
         l[0]+=1
         for num in nums:
@@ -9,3 +26,4 @@ class Solution:
                 newl[i-num]+=l[i]
             l=newl
         return l[target]
+"""   
