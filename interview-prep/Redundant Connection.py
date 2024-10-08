20dd85410c650df8c2d899b6f2e91883fe3445a6
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-10-07 21:33:07 -0700

    Create Redundant Connection.py

---
+++ interview-prep/Redundant Connection.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        graph = defaultdict(list)
+
+        def path_exists(u,v):
+            if u==v: 
+                return True
+            visited.add(u)
+
+            for neighbor in graph[u]:
+                if neighbor not in visited: 
+                    if path_exists(neighbor, v):
+                        return True
+            return False
+        
+        for u,v in edges: 
+            visited = set()
+            if path_exists(u, v):
+                return [u,v]
+            else: 
+                graph[u].append(v)
+                graph[v].append(u)
+        return None
