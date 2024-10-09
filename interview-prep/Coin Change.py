9a4c4fd78c9c45aa1d74b7bb4e3a0ab4409168fc
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-10-09 16:07:01 -0700

    Create Coin Change.py

---
+++ interview-prep/Coin Change.py
@@ -0,0 +1,11 @@
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        dp = [float('inf')] * (amount + 1)
+        dp[0] = 0
+
+        for c in coins:
+            for i in range(c, amount + 1):
+                dp[i] = min(dp[i], dp[i-c] + 1)
+        
+        return dp[amount] if dp[amount] != float('inf') else -1
