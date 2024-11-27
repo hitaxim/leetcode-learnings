4c1609716a120b3084bc19413794d21a06e2c446
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-11-27 10:18:05 -0700

    Create Number of 1 Bits.py

---
+++ interview-prep/Number of 1 Bits.py
@@ -0,0 +1,7 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        res = 0
+        for i in range(32):
+            if (n >> i ) & 1:
+                res += 1
+        return res
