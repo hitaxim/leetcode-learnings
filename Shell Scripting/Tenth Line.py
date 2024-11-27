855470bd5c586df4eb6736dd6feb70b397b4d745
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-11-27 10:26:46 -0700

    Update Tenth Line.py

--- Shell Scripting/Tenth Line.py
+++ Shell Scripting/Tenth Line.py
@@ -1,3 +1,9 @@
 # Read from the file file.txt and output the tenth line to stdout.
 #cat file.txt | tail -n +10 | head -n 1
-sed -n '10p' file.txt
+#sed -n '10p' file.txt
+
+#!/bin/bash
+filename="file.txt"
+if [ $(wc -l < "$filename") -ge 10 ]; then
+    head -n 10 "$filename" | tail -n 1
+fi
