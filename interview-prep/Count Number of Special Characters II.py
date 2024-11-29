3bb3c80df3fb2788ab09b66c3cf80f9904ed2257
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-11-28 18:12:24 -0700

    Update Count Number of Special Characters II.py

--- interview-prep/Count Number of Special Characters II.py
+++ interview-prep/Count Number of Special Characters II.py
@@ -2,16 +2,17 @@ class Solution:
     def numberOfSpecialChars(self, word: str) -> int:
         wordSet = set(word)
         dic = {}
-        special = 0
-        for i, char in enumerate(word):
-            if char.isupper() and char.lower() in wordSet:
-                if char not in dic:
-                    dic[char] = i
         
-        for i, char in enumerate(word):
-            if char.islower() and char.upper() in dic:
-                if i > dic[char.upper()]:
-                    dic.pop(char.upper())
+        ## SAVE UPPER ONES IN DICT WITH INDEX
+        for i, ch in enumerate(word):
+            if ch.isupper() and ch.lower() in wordSet:
+                if ch not in dic:
+                    dic[ch] = i
+
+        # CHECK FOR LOWER LETTERS AND COMPARE INDEX
+        for i, ch in enumerate(word):
+            if ch.islower() and ch.upper() in dic:
+                if i > dic[ch.upper()]:
+                    dic.pop(ch.upper())
         
         return len(dic)
-        
