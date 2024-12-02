5e089e2e10284a39e452a9ab5081420fec880c28
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-12-02 15:55:01 -0700

    Update Minimum Window Substring.py

--- leetcode-150/Minimum Window Substring.py
+++ leetcode-150/Minimum Window Substring.py
@@ -1,4 +1,27 @@
 class Solution:
+  def minWindow(self, s: str, t: str) -> str:
+    count = collections.Counter(t)
+    required = len(t)
+    bestLeft = -1
+    minLength = len(s) + 1
+    l = 0
+    for r, c in enumerate(s):
+      count[c] -= 1
+      if count[c] >= 0:
+        required -= 1
+      while required == 0:
+        if r - l + 1 < minLength:
+          bestLeft = l
+          minLength = r - l + 1
+        count[s[l]] += 1
+        if count[s[l]] > 0:
+          required += 1
+        l += 1
+
+    return '' if bestLeft == -1 else s[bestLeft: bestLeft + minLength]
+"""
+
+class Solution:
     def minWindow(self, s: str, t: str) -> str:
         if len(s) < len(t):
             return ""
@@ -28,4 +51,4 @@ class Solution:
                 start += 1
         
         return "" if min_len == float('inf') else s[min_start:min_start + min_len]
-        
+"""        
