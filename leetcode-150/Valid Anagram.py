b2e7e9c0abea6ea52c78d5ab5280333373d752a6
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-12-02 16:53:31 -0700

    Create Valid Anagram.py

---
+++ leetcode-150/Valid Anagram.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t): 
+            return False
+        if set(s) != set(t):
+            return False
+
+        s = sorted(s)
+        t = sorted(t)
+        for i in range(len(s)): 
+            if t[i] != s[i]:
+                return False
+        return True
