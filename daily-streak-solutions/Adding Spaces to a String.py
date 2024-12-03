d81ac375ffc1c5da1422319b80b8b0510a852a59
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-12-02 17:51:12 -0700

    Update Adding Spaces to a String.py

--- daily-streak-solutions/Adding Spaces to a String.py
+++ daily-streak-solutions/Adding Spaces to a String.py
@@ -1,5 +1,11 @@
 class Solution:
     def addSpaces(self, s: str, spaces: List[int]) -> str:
+        spaces += [len(s), 0]
+        return " ".join(s[spaces[i-1]:spaces[i]] for i in range(len(spaces)-1))
+
+"""
+class Solution:
+    def addSpaces(self, s: str, spaces: List[int]) -> str:
         result = []
         i, j = 0, 0
         n, m = len(s), len(spaces)
@@ -16,3 +22,4 @@ class Solution:
             i += 1
         
         return ''.join(result)
+"""
