ae9d26e4a27da5609b21e156c0a728cabfa6e1dc
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-12-07 08:12:49 -0700

    Create Minimum Limit of Balls in a Bag.py

---
+++ daily-streak-solutions/Minimum Limit of Balls in a Bag.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumSize(self, nums: List[int], maxOps: int) -> int:
+        low, high = 1, max(nums) # min and max possible bags
+        while low < high:
+            mid = (low + high) // 2
+            if sum((n - 1) // mid for n in nums) <= maxOps: high = mid
+            else: low = mid + 1
+        return high
+
+
