33a1cafb34b99e9694ef77073a5f7034d42b60c3
Hitaxi Mistry <hitaximistry5@gmail.com>
2024-12-09 12:02:54 -0700

    Update Smallest Good Base.py

--- leetcode-hard/Smallest Good Base.py
+++ leetcode-hard/Smallest Good Base.py
@@ -3,6 +3,17 @@ class Solution:
         n = int(n)
         for m in range(int(math.log(n, 2)), 1, -1):
             k = int(n ** m ** -1)
+            if (k ** (m + 1) - 1)
+                return str(k)
+        return str(n - 1)
+
+"""
+class Solution:
+    def smallestGoodBase(self, n: str) -> str:
+        n = int(n)
+        for m in range(int(math.log(n, 2)), 1, -1):
+            k = int(n ** m ** -1)
             if (k ** (m + 1) - 1) // (k - 1) == n:
                 return str(k)
         return str(n - 1)
+"""
