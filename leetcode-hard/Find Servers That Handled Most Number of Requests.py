e709391bf3fbd0cde54bb19f3fbac63dc3cbd97b
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-01-06 19:17:10 -0700

    Create Find Servers That Handled Most Number of Requests.py

---
+++ leetcode-hard/Find Servers That Handled Most Number of Requests.py
@@ -0,0 +1,18 @@
+class Solution:
+    def busiestServers(self, k: int, arrival: List[int], load: List[int]) -> List[int]:
+        available = list(range(k))
+        busy = []
+        res = [0] * k 
+        for i, a in enumerate(arrival):
+            while busy and busy[0][0] <= a:
+                _, x = heapq.heappop(busy)
+                heapq.heappush(available, i + (x - i) % k)
+            if available :
+                j = heapq.heappop(available) % k
+                heapq.heappush(busy, (a + load[i], j))
+                res[j] += 1
+        
+        a = max(res)
+        return [i for i in range(k) if res[i] == a]
+
+        
