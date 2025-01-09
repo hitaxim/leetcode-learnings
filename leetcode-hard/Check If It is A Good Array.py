81ee891c81fadd995ee762b3dd90370ec3008e37
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-01-08 21:21:49 -0700

    Create Check If It is A Good Array.py

---
+++ leetcode-hard/Check If It is A Good Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isGoodArray(self, nums: List[int]) -> bool:
+        ans = nums[0]
+        for i in range(1, len(nums)):
+            ans = math.gcd(ans, nums[i])
+            
+        return ans == 1
+        
