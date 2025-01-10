55c967fa5191b64c1d68bb6f43dcf083ac225260
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-01-09 20:21:04 -0700

    Create Design HashSet.py

---
+++ interview-prep-more/Design HashSet.py
@@ -0,0 +1,19 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.d = {}
+
+    def add(self, key: int) -> None:
+        self.d[key] = 1
+
+    def remove(self, key: int) -> None:
+        self.d[key] = 0
+
+    def contains(self, key: int) -> bool:
+        return self.d.get(key,0)!=0
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
