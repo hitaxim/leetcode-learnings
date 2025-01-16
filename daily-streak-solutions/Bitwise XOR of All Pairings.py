520797eb5758ca0c3f0a69a89183718be3aa3b36
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-01-15 17:09:32 -0700

    Create Bitwise XOR of All Pairings.py

---
+++ daily-streak-solutions/Bitwise XOR of All Pairings.py
@@ -0,0 +1,11 @@
+class Solution:
+    def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+        n1, n2 = len(nums1), len(nums2)
+        ans = 0
+        if n2 % 2 == 1: 
+            for i in range(n1):
+                ans = ans ^ nums1[i]
+        if n1 % 2 ==1: 
+            for i in range(n2):
+                ans = ans ^ nums2[i]
+        return ans
