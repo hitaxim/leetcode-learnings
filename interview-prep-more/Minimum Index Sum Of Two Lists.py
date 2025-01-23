b0d1d10ec51a35b8210b54988e5b170f7acaf749
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-01-22 17:06:03 -0700

    Update Minimum Index Sum Of Two Lists.py

--- interview-prep-more/Minimum Index Sum Of Two Lists.py
+++ interview-prep-more/Minimum Index Sum Of Two Lists.py
@@ -1,5 +1,24 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+        hash_one = {}
+        hash_two = {}
+        for index in range(len(list1)):
+            hash_one[list1[index]] = index
+        
+        for index in range(len(list2)):
+            hash_two[list2[index]] = index
+        
+        sums = defaultdict(list)
+        for key, value in hash_one.items():
+            if key in hash_two:
+                index_sum = value + hash_two[key]
+                sums[index_sum].append(key)
+        return sums[min(sums)]
+
+"""
+
+class Solution:
+    def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
         count = {}
         for i in range(len(list1)):
             for j in range(len(list2)):
@@ -12,3 +31,5 @@ class Solution:
             if val == min_indexsum:
                 ans.append(key)
         return ans
+
+    """
