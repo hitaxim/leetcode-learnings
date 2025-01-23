68d958c18720bae31a2d7cf80acb33bb28ac5bfe
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-01-22 17:02:01 -0700

    Update Accounts Merge.py

--- interview-prep-more/Accounts Merge.py
+++ interview-prep-more/Accounts Merge.py
@@ -24,3 +24,30 @@ class Solution:
                     self.dfs(graph, email, visit)
                     ans.append([name]+sorted(self.res))
         return ans
+"""
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        graph = collections.defaultdict(set)
+        for acc in accounts:
+            for email in acc[1:]:
+                graph[acc[1]].add(email)
+                graph[email].add(acc[1])
+        
+        res = []
+        visit = set()
+
+        for acc in accounts:
+            if acc[1] not in visit:
+                name = acc[0]
+                stack = [acc[1]]
+                emails = []
+                while stack:
+                    node = stack.pop()
+                    if node not in visit:
+                        visit.add(node)
+                        stack.extend(graph[node])
+                        emails.append(node)
+
+                res.append([name] + sorted(emails))
+        return res
+"""
