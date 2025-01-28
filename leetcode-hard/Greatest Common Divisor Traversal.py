a77dc8d11db163cd718f3338ec1309bd395560dc
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-01-27 17:54:59 -0700

    Update Greatest Common Divisor Traversal.py

--- leetcode-hard/Greatest Common Divisor Traversal.py
+++ leetcode-hard/Greatest Common Divisor Traversal.py
@@ -1,35 +1,17 @@
 class Solution:
-    def dfs(self, index, visitedIndex, visitedPrime):
-        if visitedIndex[index]:
-            return
-        visitedIndex[index] = True
-
-        for prime in self.index2prime[index]:
-            if visitedPrime.get(prime, False):
-                continue
-            visitedPrime[prime] = True
-            for index1 in self.prime2index[prime]:
-                if visitedIndex[index1]:
-                    continue
-                self.dfs(index1, visitedIndex, visitedPrime)
-
     def canTraverseAllPairs(self, nums: List[int]) -> bool:
-        self.prime2index = {}
-        self.index2prime = {}
-        for i, num in enumerate(nums):
-            temp = num
-            for j in range(2, int(num ** 0.5) + 1):
-                if temp % j == 0:
-                    self.prime2index.setdefault(j, []).append(i)
-                    self.index2prime.setdefault(i, []).append(j)
-                    while temp % j == 0:
-                        temp //= j
-            if temp > 1:
-                self.prime2index.setdefault(temp, []).append(i)
-                self.index2prime.setdefault(i, []).append(temp)
+        if len(nums)==1:return True
+        nums = set(nums)
+        if 1 in nums:return False
+        if len(nums)==1:return True
+        
+        nums = sorted(nums,reverse=True)
 
-        visitedIndex = [False] * len(nums)
-        visitedPrime = {}
-        self.dfs(0, visitedIndex, visitedPrime)
-
-        return all(visitedIndex)
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if gcd(nums[i],nums[j])-1:
+                    nums[j]*=nums[i]
+                    break
+            else:
+                return False
+        return True
