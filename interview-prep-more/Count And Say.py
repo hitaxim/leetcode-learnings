92ce7a8afc9f6aefca9e603d78a018bf1e407aff
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-01-29 18:48:00 -0700

    Create Count And Say.py

---
+++ interview-prep-more/Count And Say.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countAndSay(self, n: int) -> str:
+        if n == 1:
+            return "1"
+        elif n == 2:
+            return "11"
+        s = self.countAndSay(n - 1)
+        result = []
+        count = 1
+        for i in range(1, len(s)):
+            if s[i] == s[i - 1]:
+                count += 1
+            else:
+                result.append(str(count) + s[i - 1])
+                count = 1
+        result.append(str(count) + s[-1])
+        return "".join(result)
