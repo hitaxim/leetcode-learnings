3f28b07ea5b5718952cdd25cad088843a4370c42
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-01-30 19:16:45 -0700

    Create Largest Palindromic Number.py

---
+++ interview-prep-more/Largest Palindromic Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def largestPalindromic(self, num: str) -> str:
+        count = Counter(num)
+        palindrome = mid = ''
+
+        for d in sorted(count.keys(), reverse=True):
+            mid = max(mid, d * (count[d] & 1))
+            palindrome += d * (count[d] // 2)
+
+        palindrome = palindrome.lstrip('0')
+        return (palindrome + mid + palindrome[::-1]) or '0'
+    
+      
