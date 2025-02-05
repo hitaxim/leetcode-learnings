a8b5b594f6d00947b76695a26805462773563002
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-02-05 10:42:50 -0700

    Create Queries Quality And Percentage.sql

---
+++ leetcode-sql-50/Queries Quality And Percentage.sql
@@ -0,0 +1,8 @@
+
+# Write your MySQL query statement below
+SELECT query_name, 
+ROUND(AVG(CAST(rating AS DECIMAL)/ position), 2) AS quality, 
+ROUND(SUM( CASE WHEN rating<3 THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS poor_query_percentage
+FROM Queries
+WHERE query_name IS NOT NULL
+GROUP BY query_name
