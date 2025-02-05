5b7026a45d1db7850463af0c32454e10ca626fab
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-02-05 12:55:38 -0700

    Create Consecutive Numbers.sql

---
+++ leetcode-sql-50/Consecutive Numbers.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+WITH CTE AS (
+    SELECT num, 
+    LAG(num,1) OVER() num1,
+    LEAD(num,1) OVER()  num2
+    FROM Logs
+)
+
+SELECT DISTINCT num AS ConsecutiveNums
+FROM CTE 
+WHERE num1 = num AND num = num2
