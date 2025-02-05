e1583c6947f45742cb8046dfa1c318b55c00409f
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-02-05 13:26:37 -0700

    Create Exchange Seats.sql

---
+++ leetcode-sql-50/Exchange Seats.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT id, CASE 
+    WHEN id % 2 = 0 THEN LAG(student) OVER(ORDER BY id)
+    ELSE COALESCE(LEAD(student) OVER(ORDER BY id), student)
+    END AS student
+FROM Seat
