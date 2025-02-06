230491ab2256b555fcc2383df277bffc30a796dc
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-02-05 21:35:48 -0700

    Create TikTok - Signup Activation Rate.sql

---
+++ SQL-interview-questions/TikTok - Signup Activation Rate.sql
@@ -0,0 +1,7 @@
+SELECT 
+  ROUND(COUNT(t.email_id)::DECIMAL 
+  /COUNT(DISTINCT e.email_id),2) AS activation_rate
+FROM emails e 
+LEFT JOIN texts t 
+ON e.email_id = t.email_id 
+AND t.signup_action = 'Confirmed';
