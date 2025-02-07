7f214cb730865970f2d84afa0f6ce10dd1edc93b
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-02-06 19:29:10 -0700

    Create Rearrange Products Table.py

---
+++ 30-Days-Of-Pandas/Rearrange Products Table.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
+    return pd.melt(
+        products, id_vars='product_id', var_name='store', value_name='price'
+    ).dropna()
+
+
+"""
+# Write your MySQL query statement below
+SELECT product_id, 'store1' AS store, store1 AS price
+FROM Products
+WHERE store1 IS NOT NULL
+UNION
+SELECT product_id, 'store2' AS store, store2 AS price
+FROM Products
+WHERE store2 IS NOT NULL
+UNION
+SELECT product_id, 'store3' AS store, store3 AS price
+FROM Products
+WHERE store3 IS NOT NULL
+"""
