a44a2ea92ad43052bf4f4de9fecdc3ccc683e2fb
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-02-11 17:18:30 -0700

    Update Search a 2D Matrix II.py

--- interview-prep-more/Search a 2D Matrix II.py
+++ interview-prep-more/Search a 2D Matrix II.py
@@ -16,4 +16,34 @@ class Solution:
                 i-=1
                 
         return False
+
+"""
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        output = False
+        for lst in matrix:
+            if lst[0] <= target and target <= lst[-1]:
+                if target in lst:
+                    output = True
+                    break
         
+        return output
+
+
+class Solution:
+    def checkRow(self, arr: List[int], target: int) -> bool:
+        for i in arr:
+            if i < target:
+                continue
+            elif i > target:
+                return False
+            else:
+                return True
+
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        for arr in matrix:
+            if self.checkRow(arr, target):
+                return True
+            continue
+        return False
+"""
