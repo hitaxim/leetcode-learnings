414d50e0e696ddbbb7cef8d429e109f031cfd7f3
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-02-11 17:17:15 -0700

    Create Search a 2D Matrix II.py

---
+++ interview-prep-more/Search a 2D Matrix II.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchMatrix(self, mat: List[List[int]], target: int) -> bool:
+        #return any(target in row for row in matrix)
+        m=len(mat)
+        n=len(mat[0])
+        
+        i=m-1
+        j=0
+        
+        while i>=0 and j<n:
+            if mat[i][j]==target:
+                return True
+            elif mat[i][j]<target:
+                j+=1
+            else:
+                i-=1
+                
+        return False
+        
