213910fa70ee61ed13f12bd33839164fcb292596
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-02-17 18:12:32 -0700

    Create Binary Tree Tilt.py

---
+++ interview-prep-more/Binary Tree Tilt.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findTilt(self, root: Optional[TreeNode]) -> int:
+        self.total_tilt = 0
+        
+        def calculate_tilt(node):
+            if not node:
+                return 0
+            
+            left_sum = calculate_tilt(node.left)
+            right_sum = calculate_tilt(node.right)
+            tilt = abs(left_sum - right_sum)
+            
+            self.total_tilt += tilt
+            
+            return left_sum + right_sum + node.val
+        
+        calculate_tilt(root)
+        return self.total_tilt
