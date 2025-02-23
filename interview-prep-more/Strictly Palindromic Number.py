0c0287ee2a83fbcb7cb468e0aaf5f3621560209f
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-02-22 19:35:01 -0700

    Create Strictly Palindromic Number.py

---
+++ interview-prep-more/Strictly Palindromic Number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isStrictlyPalindromic(self, n: int) -> bool:
+        def int2base(n,b):
+            r=""
+            while n>0:
+                r+=str(n%b)
+                n//=b           
+            return int(r[-1::-1])
+        for i in range(2,n-1):
+            if int2base(n,i)!=n:
+                return False
+        return True
