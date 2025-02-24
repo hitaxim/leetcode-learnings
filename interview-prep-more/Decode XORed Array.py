5f7beb42d06edf551ac96e602f64a7b45de2757e
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-02-23 21:12:08 -0700

    Create Decode XORed Array.py

---
+++ interview-prep-more/Decode XORed Array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        res = [first]
+        for num in encoded:
+            res.append(res[-1] ^ num)
+        return res
+        
