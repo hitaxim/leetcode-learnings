6a016ac477542fc6e79a0acff50bad06a9f70c2c
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-02-28 13:21:33 -0700

    Update AQR - Pearson Correlation Coefficient.py

--- Python-Interview-Questions/AQR - Pearson Correlation Coefficient.py
+++ Python-Interview-Questions/AQR - Pearson Correlation Coefficient.py
@@ -1,24 +1,5 @@
 import math
 
-def mean(x):
-	return sum(x)/len(x)
-	
-def sd(x):
-  m = mean(x)
-  ss = sum((i-m) ** 2 for i in x)
-  return math.sqrt(ss / len(x))
-
-def corr(x, y):
-  x_m = mean(x)
-  y_m = mean(y)
-  xy_d = [] # product of de-meaned X and Y, for covariance calc
-  for i in range(len(x)):
-    x_d = x[i] - x_m
-    y_d = y[i] - y_m
-    xy_d.append(x_d * y_d) # add product of X_i and Y_i 
-  return mean(xy_d) / (sd(x) * sd(y)) # from formula above
-
-"""
 def corr(x, y):
   
   def mean(x):
@@ -39,4 +20,3 @@ def corr(x, y):
   
   res = cov(x,y) / (sqrt_var(x) * sqrt_var(y))
   return res
-"""
