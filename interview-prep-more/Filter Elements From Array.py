c91aebdefcd609346b75ad9c412d40e6137ce64e
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-02-28 10:41:09 -0700

    Create Filter Elements From Array.py

---
+++ interview-prep-more/Filter Elements From Array.py
@@ -0,0 +1,9 @@
+function filter(arr, fn) {
+    let filteredArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i)) {
+            filteredArr.push(arr[i]);
+        }
+    }
+    return filteredArr;
+}
