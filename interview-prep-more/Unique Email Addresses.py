023ea6e0450835c72ccffe0a38dad932aeb527c5
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-01 19:46:20 -0700

    Create Unique Email Addresses.py

---
+++ interview-prep-more/Unique Email Addresses.py
@@ -0,0 +1,9 @@
+class Solution:
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        unique_emails = set()
+        for email in emails:
+            local, domain = email.split('@')
+            local = local.split('+')[0]
+            local = local.replace('.', '')
+            unique_emails.add((local, domain))
+        return len(unique_emails)
