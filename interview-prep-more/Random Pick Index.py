687d70f4ff149f43dace4a5294e3feda35fc0b49
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-02 20:26:56 -0700

    Create Random Pick Index.py

---
+++ interview-prep-more/Random Pick Index.py
@@ -0,0 +1,9 @@
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.indices = defaultdict(list)
+        for i, num in enumerate(nums):
+            self.indices[num].append(i)
+
+    def pick(self, target: int) -> int:
+        return random.choice(self.indices[target])
