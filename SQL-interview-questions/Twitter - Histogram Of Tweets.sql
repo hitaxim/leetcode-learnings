9fdc2376d54df40a7b9abc6d254bbb51a443c855
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-05 14:37:45 -0700

    Update Twitter - Histogram Of Tweets.sql

--- SQL-interview-questions/Twitter - Histogram Of Tweets.sql
+++ SQL-interview-questions/Twitter - Histogram Of Tweets.sql
@@ -10,3 +10,17 @@ SELECT tweet_count_per_user AS tweet_bucket,
   COUNT(user_id) AS users_num
   FROM total_tweets
   GROUP BY tweet_count_per_user;
+
+"""
+  WITH CTE AS (
+  SELECT COUNT(tweet_id) AS tweet_counts, user_id
+  FROM tweets
+  WHERE EXTRACT(year FROM tweet_date) = 2022
+  GROUP BY user_id
+)
+
+SELECT tweet_counts AS tweet_bucket, 
+COUNT(user_id) AS users_num
+FROM CTE
+GROUP BY tweet_counts;
+  """
