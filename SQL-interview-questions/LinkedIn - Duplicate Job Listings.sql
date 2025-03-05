0a6bd2dabb03d3b1935ebec73cd2f1f2fdb0a185
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-05 16:16:02 -0700

    Create LinkedIn - Duplicate Job Listings.sql

---
+++ SQL-interview-questions/LinkedIn - Duplicate Job Listings.sql
@@ -0,0 +1,28 @@
+WITH job_count_cte AS (
+  SELECT 
+    company_id, 
+    title, 
+    description, 
+    COUNT(job_id) AS job_count
+  FROM job_listings
+  GROUP BY company_id, title, description
+)
+
+SELECT COUNT(DISTINCT company_id) AS duplicate_companies
+FROM job_count_cte
+WHERE job_count > 1;
+
+/*
+SELECT COUNT(DISTINCT company_id) AS duplicate_companies
+FROM (
+  SELECT 
+    company_id, 
+    title, 
+    description, 
+    COUNT(job_id) AS job_count
+  FROM job_listings
+  GROUP BY company_id, title, description
+) AS job_count_cte
+WHERE job_count > 1;
+
+*/
