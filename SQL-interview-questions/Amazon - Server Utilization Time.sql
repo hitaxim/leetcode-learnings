c8b55bb03d7e1df9ee587e48143c912da389c322
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-05 15:17:09 -0700

    Create Amazon - Server Utilization Time.sql

---
+++ SQL-interview-questions/Amazon - Server Utilization Time.sql
@@ -0,0 +1,17 @@
+WITH running_time 
+AS (
+  SELECT
+    server_id,
+    session_status,
+    status_time AS start_time,
+    LEAD(status_time) OVER (
+      PARTITION BY server_id
+      ORDER BY status_time) AS stop_time
+  FROM server_utilization
+)
+
+SELECT
+  DATE_PART('days', JUSTIFY_HOURS(SUM(stop_time - start_time))) AS total_uptime_days
+FROM running_time
+WHERE session_status = 'start'
+  AND stop_time IS NOT NULL;
