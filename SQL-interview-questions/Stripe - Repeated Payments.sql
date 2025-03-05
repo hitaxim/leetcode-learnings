08c1abc78c2800e2260974fe00fcfe64085318d6
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-05 15:36:48 -0700

    Update Stripe - Repeated Payments.sql

--- SQL-interview-questions/Stripe - Repeated Payments.sql
+++ SQL-interview-questions/Stripe - Repeated Payments.sql
@@ -1,1 +1,13 @@
-sdgfdfh
+WITH payments AS (
+  SELECT 
+    merchant_id, 
+    EXTRACT(EPOCH FROM transaction_timestamp - 
+      LAG(transaction_timestamp) OVER(
+        PARTITION BY merchant_id, credit_card_id, amount 
+        ORDER BY transaction_timestamp)
+    )/60 AS minute_difference 
+  FROM transactions) 
+
+SELECT COUNT(merchant_id) AS payment_count
+FROM payments 
+WHERE minute_difference <= 10;
