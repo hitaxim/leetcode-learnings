ec412112ec0d5b2867d51a5155827cba62803a88
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-04 18:53:18 -0700

    Create Count Total Number Of Colored Cells.py

---
+++ daily-streak-solutions/Count Total Number Of Colored Cells.py
@@ -0,0 +1,17 @@
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        return 2*n*(n-1)+1
+
+"""
+class Solution:
+    def coloredCells(self, n: int) -> int:
+        res=1
+
+        i=0
+        while i<n:
+            res=res+(4*i)
+            i+=1
+            
+        return(res)
+        
+"""
