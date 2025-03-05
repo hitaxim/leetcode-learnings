02225b1b903b6732eea63ebaad9ac551e4807c33
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-05 15:44:20 -0700

    Create McKinsey - 3-Topping Pizzas.sql

---
+++ SQL-interview-questions/McKinsey - 3-Topping Pizzas.sql
@@ -0,0 +1,23 @@
+SELECT 
+  CONCAT(p1.topping_name, ',', p2.topping_name, ',', p3.topping_name) AS pizza,
+  p1.ingredient_cost + p2.ingredient_cost + p3.ingredient_cost AS total_cost
+FROM pizza_toppings AS p1
+INNER JOIN pizza_toppings AS p2
+  ON p1.topping_name < p2.topping_name 
+INNER JOIN pizza_toppings AS p3
+  ON p2.topping_name < p3.topping_name 
+ORDER BY total_cost DESC, pizza;
+
+/*
+SELECT 
+  CONCAT(p1.topping_name, ',', p2.topping_name, ',', p3.topping_name) AS pizza,
+  p1.ingredient_cost + p2.ingredient_cost + p3.ingredient_cost AS total_cost
+FROM pizza_toppings AS p1
+CROSS JOIN
+  pizza_toppings AS p2,
+  pizza_toppings AS p3
+WHERE p1.topping_name < p2.topping_name
+  AND p2.topping_name < p3.topping_name
+ORDER BY total_cost DESC, pizza;
+*/
+
