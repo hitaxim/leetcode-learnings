da113d8d23e4ad20c284b8b6f07d9aaed60b6b06
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-05 15:09:40 -0700

    Update TikTok - Signup Activation Rate.sql

--- SQL-interview-questions/TikTok - Signup Activation Rate.sql
+++ SQL-interview-questions/TikTok - Signup Activation Rate.sql
@@ -5,3 +5,12 @@ FROM emails e
 LEFT JOIN texts t 
 ON e.email_id = t.email_id 
 AND t.signup_action = 'Confirmed';
+
+"""
+SELECT ROUND(CAST(COUNT(t.email_id) AS DECIMAL)/COUNT(DISTINCT e.email_id),2) AS activation_rate
+FROM emails e 
+LEFT JOIN texts t
+ON e.email_id = t.email_id
+AND t.signup_action = 'Confirmed';
+
+  """
