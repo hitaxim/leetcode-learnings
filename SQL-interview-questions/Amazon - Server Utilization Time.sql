de0e48ea5eafce0b6c78e6143d9029a23c3ed0ff
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-05 15:27:10 -0700

    Update Amazon - Server Utilization Time.sql

--- SQL-interview-questions/Amazon - Server Utilization Time.sql
+++ SQL-interview-questions/Amazon - Server Utilization Time.sql
@@ -15,3 +15,19 @@ SELECT
 FROM running_time
 WHERE session_status = 'start'
   AND stop_time IS NOT NULL;
+
+"""
+  WITH server_sessions AS (
+    SELECT 
+        server_id, 
+        status_time AS start_time,
+        LEAD(status_time) OVER (PARTITION BY server_id ORDER BY status_time) AS stop_time
+    FROM server_utilization
+    WHERE session_status = 'start'
+)
+SELECT 
+    FLOOR(SUM(EXTRACT(EPOCH FROM (stop_time - start_time))) / 86400) AS total_uptime_days
+FROM server_sessions
+WHERE stop_time IS NOT NULL;
+
+"""
