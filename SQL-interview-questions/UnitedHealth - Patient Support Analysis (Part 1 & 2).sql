3580b2583e1ae4e6785639286f022bff745a57f7
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-17 15:55:08 -0700

    Update and rename UnitedHealth - Patient Support Analysis (Part 1).sql to UnitedHealth - Patient Support Analysis (Part 1 & 2).sql

--- SQL-interview-questions/UnitedHealth - Patient Support Analysis (Part 1 & 2).sql
+++ SQL-interview-questions/UnitedHealth - Patient Support Analysis (Part 1 & 2).sql
@@ -21,3 +21,17 @@ WITH call_records AS (
 
 SELECT COUNT(policy_holder_id) AS policy_holder_count
 FROM call_records;
+
+
+------ PART - 2 ---------
+WITH uncategorised_callers AS (
+  SELECT COUNT(case_id) AS uncategorised_calls
+  FROM callers
+  WHERE call_category IS NULL
+    OR call_category = 'n/a'
+)
+
+SELECT 
+  ROUND(100.0 * uncategorised_calls 
+    / (SELECT COUNT(*) FROM callers), 1) AS uncategorised_call_pct
+FROM uncategorised_callers;
