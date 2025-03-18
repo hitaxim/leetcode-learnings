a2a23ec9b1d4e209c25493da84c63a70bb5e42ba
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-17 17:20:51 -0700

    Create Amazon - User Shopping Spree.sql

---
+++ SQL-interview-questions/Amazon - User Shopping Spree.sql
@@ -0,0 +1,7 @@
+SELECT DISTINCT T1.user_id
+FROM transactions AS T1
+INNER JOIN transactions AS T2
+  ON DATE(T2.transaction_date) = DATE(T1.transaction_date) + 1
+INNER JOIN transactions AS T3
+  ON DATE(T3.transaction_date) = DATE(T1.transaction_date) + 2
+ORDER BY T1.user_id;
