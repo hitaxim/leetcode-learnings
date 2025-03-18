d15959c154f0a8b4aaf86bc05325a2e4334b0c9d
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-17 19:04:49 -0700

    Create Google - Median Search Frequency.sql

---
+++ SQL-interview-questions/Google - Median Search Frequency.sql
@@ -0,0 +1,11 @@
+WITH searches_expanded AS (
+  SELECT searches
+  FROM search_frequency
+  GROUP BY 
+    searches, 
+    GENERATE_SERIES(1, num_users))
+
+SELECT 
+  ROUND(PERCENTILE_CONT(0.50) WITHIN GROUP (
+    ORDER BY searches)::DECIMAL, 1) AS  median
+FROM searches_expanded;
