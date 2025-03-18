b846368723c0b23b4be9c621a6c11729f9117df0
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-17 19:01:38 -0700

    Create Facebook - Active User Retention.sql

---
+++ SQL-interview-questions/Facebook - Active User Retention.sql
@@ -0,0 +1,14 @@
+SELECT 
+  EXTRACT(MONTH FROM curr_month.event_date) AS mth, 
+  COUNT(DISTINCT curr_month.user_id) AS monthly_active_users 
+FROM user_actions AS curr_month
+WHERE EXISTS (
+  SELECT last_month.user_id 
+  FROM user_actions AS last_month
+  WHERE last_month.user_id = curr_month.user_id
+    AND EXTRACT(MONTH FROM last_month.event_date) =
+    EXTRACT(MONTH FROM curr_month.event_date - interval '1 month')
+)
+  AND EXTRACT(MONTH FROM curr_month.event_date) = 7
+  AND EXTRACT(YEAR FROM curr_month.event_date) = 2022
+GROUP BY EXTRACT(MONTH FROM curr_month.event_date);
