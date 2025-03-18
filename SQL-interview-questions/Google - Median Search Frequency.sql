5db92d2dad1c089c248b4b4fc7d20155951551d0
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-17 19:06:44 -0700

    Update Google - Median Search Frequency.sql

--- SQL-interview-questions/Google - Median Search Frequency.sql
+++ SQL-interview-questions/Google - Median Search Frequency.sql
@@ -9,3 +9,27 @@ SELECT
   ROUND(PERCENTILE_CONT(0.50) WITHIN GROUP (
     ORDER BY searches)::DECIMAL, 1) AS  median
 FROM searches_expanded;
+
+
+--- SOLUTION 2 -----
+WITH cumulative_users AS (
+    SELECT 
+        searches, 
+        num_users, 
+        SUM(num_users) OVER (ORDER BY searches) AS running_total,
+        SUM(num_users) OVER () AS total_users
+    FROM search_frequency
+)
+SELECT ROUND(
+    CASE 
+        WHEN total_users % 2 = 1 THEN 
+            (SELECT searches FROM cumulative_users 
+             WHERE running_total >= (total_users + 1) / 2 
+             ORDER BY running_total 
+             LIMIT 1)
+        ELSE 
+            (SELECT AVG(searches * 1.0) FROM cumulative_users 
+             WHERE running_total BETWEEN total_users / 2 AND (total_users / 2 + 1))
+    END, 1) AS median
+FROM cumulative_users
+LIMIT 1;
