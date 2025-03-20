84886971dd239ed388560d6da8273cd5e2dc50d2
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-20 15:32:36 -0700

    Update Google Maps Flagged UGC.sql

--- Google-SQL-Questions/Google Maps Flagged UGC.sql
+++ Google-SQL-Questions/Google Maps Flagged UGC.sql
@@ -11,3 +11,24 @@ SELECT place_category AS off_topic_places
 FROM OffTopicCounts
 WHERE off_topic_count = (SELECT MAX(off_topic_count) FROM OffTopicCounts)
 ORDER BY place_category;
+
+
+
+/*
+WITH reviews AS (
+    SELECT 
+        place.place_category,
+        COUNT(ugc.content_id) AS content_count
+    FROM place_info place
+    JOIN maps_ugc_review ugc
+        ON place.place_id = ugc.place_id
+    WHERE ugc.content_tag = 'Off-topic'
+    GROUP BY place.place_category
+)
+SELECT 
+    place_category,
+    content_count,
+    RANK() OVER (ORDER BY content_count DESC) AS top_place
+FROM reviews;
+
+*/
