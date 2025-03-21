67126e7d6429525883b4fff2c7811b7d6eaece5e
Hitaxi Mistry <hitaximistry5@gmail.com>
2025-03-21 09:44:50 -0700

    Create Microsoft - Mean, Median, Mode.sql

---
+++ SQL-interview-questions/Microsoft - Mean, Median, Mode.sql
@@ -0,0 +1,5 @@
+SELECT 
+  ROUND(AVG(email_count)) as mean,
+  PERCENTILE_CONT(0.5) WITHIN GROUP (ORDER BY email_count) AS median,
+  MODE() WITHIN GROUP (ORDER BY email_count) AS mode
+ FROM inbox_stats;
